1f20c762dc11b556eaeb07f4a816229541de275e
corentin.f <corentin.fagherazzi@y-nov.com>
2015-10-23 16:26:09 +0200

    maj modèle users et route.js pour l'authentification

--- CFGPT_API/api/models/Users.js
+++ CFGPT_API/api/models/Users.js
@@ -33,7 +33,26 @@ module.exports = {
 	logs:{
 		collection: 'logs',
 		via: 'user'
-	}
+	},
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  },
+ 
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        }else{
+          user.password = hash;
+          cb(null, user);
+        }
+      });
+    });
   }
 };
 
