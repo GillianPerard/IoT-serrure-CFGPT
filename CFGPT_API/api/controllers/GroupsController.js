eb7120c2335d0b93ccc8351b08c94635d1cd2ef0
Udolwa34 <ludovic.caisso@y-nov.com>
2015-10-29 11:02:26 +0100

    Add "getUsers" action on GroupsController

--- CFGPT_API/api/controllers/GroupsController.js
+++ CFGPT_API/api/controllers/GroupsController.js
@@ -6,6 +6,52 @@
  */
 
 module.exports = {
-	
+
+  // Récupère les informations des users du group de l'id passé en paramètre (+ les infos associées à son groupuser)
+	getUsers: function(req,res){
+		//var plop = req.query.user;
+    var _groupId = req.param('groupId');
+
+		if (_groupId == undefined ||_groupId == "" ||_groupId == null){
+			res.send("No groupId param.");
+			return;
+		}
+
+		Groups.find({id: _groupId}).exec(function(err, group){
+			if (group == undefined || group == "" || group == null){
+				res.send("No group found.");
+				return;
+			}
+			GroupUsers.find({group: group[0]['id']}).populate('user').exec(function(err, userWithGroup){
+				if (userWithGroup == undefined || userWithGroup == "" || userWithGroup == null){
+					res.send("No groupuser found.");
+					return;
+				}
+				res.send(userWithGroup);
+			})
+		})
+	}
+
+
+
+/*
+	userByLogicielName : function(req,res){
+		Logiciels.findBy({name:'Toto Adventure!'}).populate('owner').exec(function(err,logiciels){
+			res.send(logiciels);
+		})
+
+		Logiciels.count({name:'Toto Adventure!'}).exec(function(err,count){
+			if(err) return;
+			res.send(count);
+		})
+	},
+
+	userByLogicielName : function(req,res){
+		var owner = req.query.user;
+		var name = req.param('name');
+		Logiciels.create({name:name, owner:owner}).exec(function(err,logiciel){
+		})
+	}
+*/
 };
 
