8cb5084ac9b874e7f9601ee1936b505a1dbc0f85
GillianPerard <gillian.perard@gmail.com>
2015-10-29 15:10:34 +0100

    Add subgroups to group

--- CFGPT_mobile/www/js/controllers/GroupsCtrl.js
+++ CFGPT_mobile/www/js/controllers/GroupsCtrl.js
@@ -1,4 +1,25 @@
 angular.module('CFGPT_Mobile.controllers.GroupsCtrl', [])
 	.controller('GroupsCtrl', ['$scope', 'GroupsService', function ($scope, GroupsService) {
 		$scope.groups = GroupsService.getGroups();
+		
+		$scope.showHomeIcon = function(id){
+			if(GroupsService.getGroups()[id].type == "HOUSE"){
+				return true;
+			}
+		}
+		
+		$scope.showSuitcaseIcon = function(id){
+			if(GroupsService.getGroups()[id].type == "JOB"){
+				return true;
+			}
+		}
+		
+		$scope.showRocketIcon = function(id){
+			if(GroupsService.getGroups()[id].type == "OTHER"){
+				return true;
+			}
+		}
+		
 	}]);
+	
+	
--- CFGPT_mobile/www/js/services/GroupsService.js
+++ CFGPT_mobile/www/js/services/GroupsService.js
@@ -2,12 +2,28 @@ angular.module('CFGPT_Mobile.services.GroupsService', [])
 
   .service('GroupsService', function () {
     var groups = [
-      { title: 'Maison principale', id: 0 , type: "HOUSE"},
+      { title: 'Maison principale', id: 0 , type: "HOUSE",
+        subgroups: [
+          { title: "Toutes les clés", id: 5, 
+            keysList: [
+                {title: "Portail", id: 0},
+                {title: "Porte d'entrée", id: 1},
+                {title: "Atelier", id: 2},
+                {title: "Local piscine", id: 3}
+            ]
+          },
+          { title: "Enfants", id: 6, 
+            keysList: [
+                {title: "Portail", id: 0},
+                {title: "Porte d'entrée", id: 1}
+            ]
+          }
+        ]
+      },
       { title: 'Bureau', id: 1 , type: "JOB"},
-      { title: 'Cabanne au fond du jardin', id: 2 , type: "AUTRE"},
+      { title: 'Cabanne au fond du jardin', id: 2 , type: "OTHER"},
       { title: 'Maison secondaire', id: 3 , type: "HOUSE"},
-      { title: 'Pied à terre', id: 4 , type: "HOUSE"},
-      { title: 'Cabanne au fond du jardin', id: 5 , type: "CASTEL"}
+      { title: 'Pied à terre', id: 4 , type: "HOUSE"}
     ];
 
     this.getGroups = function () {
