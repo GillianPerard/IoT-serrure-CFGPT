c55bf82ab83d0d1ef8e197ab966d8e2afac1da0e
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-10-29 09:28:10 +0100

    app : fix routes

---
+++ CFGPT_mobile/www/js/services/GroupsService.js
@@ -0,0 +1,19 @@
+angular.module('CFGPT_Mobile.services.GroupsService', [])
+
+  .service('GroupsService', function () {
+//     var playlists = [
+//       { title: 'Reggae', id: 1 },
+//       { title: 'Chill', id: 2 },
+//       { title: 'Dubstep', id: 3 },
+//       { title: 'Indie', id: 4 },
+//       { title: 'Rap', id: 5 }
+//     ];
+// 
+//     this.getPlaylists = function () {
+//       return playlists;
+//     };
+//     
+//     this.getPlaylist = function (id) {
+//       return playlists[id];
+//     }
+  });
