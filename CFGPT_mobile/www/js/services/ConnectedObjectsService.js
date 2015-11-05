99aaee597353f780c1676173105a045d308efedd
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-11-05 11:03:32 +0100

    possibilité de récupèrer toutes les clés

--- CFGPT_mobile/www/js/services/ConnectedObjectsService.js
+++ CFGPT_mobile/www/js/services/ConnectedObjectsService.js
@@ -1,20 +1,25 @@
 angular.module('CFGPT_Mobile.services.ConnectedObjectsService', [])
 
 	.service('ConnectedObjectsService', function ($http, ConstantService) {
-		this.currentConnectedObjects = undefined;
 
 		this.getConnectedObjects = function (groupId, callback) {
-			if (!this.currentConnectedObjects) {
-				$http.get(ConstantService.baseUrl + '/app/groups/' + groupId + '/connectedobjects').then(
-					function (success) {
-						currentConnectedObjects = success.data;
-						callback(currentConnectedObjects);
-					},
-					function (error) {
-						callback(undefined, error);
-					});
+
+			if (groupId < 0) {
+				$http.get(ConstantService.baseUrl + '/app/connectedobjects/').then(
+				function (success) {
+					callback(success.data);
+				},
+				function (error) {
+					callback(undefined, error);
+				});
 			} else {
-				callback(currentConnectedObjects);
+				$http.get(ConstantService.baseUrl + '/app/groups/' + groupId + '/connectedobjects').then(
+				function (success) {
+					callback(success.data);
+				},
+				function (error) {
+					callback(undefined, error);
+				});
 			}
 		};
 	});
