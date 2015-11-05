77e67a6d369abeb3db5869c783d46aeb330f3062
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-11-05 10:28:02 +0100

    correction bug index

--- CFGPT_mobile/www/js/controllers/GroupCtrl.js
+++ CFGPT_mobile/www/js/controllers/GroupCtrl.js
@@ -1,7 +1,7 @@
 angular.module('CFGPT_Mobile.controllers.GroupCtrl', [
 	'CFGPT_Mobile.services.ConnectedObjectsService'])
 	.controller('GroupCtrl', ['$scope', '$stateParams', 'ConnectedObjectsService', function ($scope, $stateParams, ConnectedObjectsService) {
-		ConnectedObjectsService.getConnectedObjects($stateParams.groupId - 1, function (result, error) {
+		ConnectedObjectsService.getConnectedObjects($stateParams.groupId, function (result, error) {
 			if (!result && error) {
 				alert(error.data.err);
 			} else {
