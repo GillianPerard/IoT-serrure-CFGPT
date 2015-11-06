60c44a97ecee604bbc4d932f45080a9ca68b31ad
GillianPerard <gillian.perard@gmail.com>
2015-11-06 11:21:24 +0100

    App : add group and remove group work

--- CFGPT_mobile/www/js/controllers/GroupsCtrl.js
+++ CFGPT_mobile/www/js/controllers/GroupsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('CFGPT_Mobile.controllers.GroupsCtrl', [
 	'CFGPT_Mobile.services.UserGroupsService'])
-	.controller('GroupsCtrl', function ($scope, UserGroupsService, APIService) {
+	.controller('GroupsCtrl', function ($scope, UserGroupsService, APIService, $ionicPopup, $state, $window) {
 		UserGroupsService.getMyUserGroups(function (result, error) {
 			if (!result && error) {
 				alert(error.err);
@@ -10,29 +10,71 @@ angular.module('CFGPT_Mobile.controllers.GroupsCtrl', [
 		});
 
 		$scope.newGroup = function () {
-			// TODO : ajouté le code pour la fenêtre modal d'ajout 
+			$scope.data = {}
+			// An elaborate, custom popup
+			var myPopup = $ionicPopup.show({
+				template: '<input type="text" ng-model="data.groupName" placeholder="Nom" style="border: 1px solid #EEE;padding:5px">',
+				title: 'Nouveau groupe',
+				scope: $scope,
+				buttons: [
+					{ text: 'Annuler',
+					  type: 'noBackground'
+					},
+					{
+						text: '<b>Ajouter</b>',
+						type: 'button-positive',
+						onTap: function (e) {
+							if (!$scope.data.groupName) {
+								//don't allow the user to close unless he enters wifi password
+								e.preventDefault();
+							} else {
+								addGroup($scope.data.groupName);
+							}
+						}
+					}
+				]
+			}); 
 		};
 
-		var addGroup = function () {
-			APIService.groups.add($scope.groupName,
+		var addGroup = function (groupName) {
+			APIService.groups.add(groupName,
 				function (data) {
-					// refreshGroups();
-					alert("Groupe ajouté");
+					$window.location.reload(true)
+					console.log("Groupe ajouté !");
 				},
 				function (error) {
 					alert(error);
 				});
-		};
-
-		$scope.removeGroup = function (userGroup) {
-			APIService.groups.add(userGroup.group.id,
+			};
+		
+		var deleteGroup = function (groupId) {
+			APIService.groups.remove(groupId,
 				function (data) {
-					// refreshGroups();
-					alert("Groupe supprimé !");
+					console.log("Groupe supprimé !");
+					$window.location.reload(true);
 				},
 				function (error) {
 					alert(error);
 				});
+			};
+
+		$scope.removeGroup = function (groupId) {
+			var confirmPopup = $ionicPopup.confirm({
+				title: 'Suppression de trousseau',
+				template: 'Êtes-vous sûr de vouloir supprimer ce trousseau ?',
+				buttons: [
+					{ text: 'Non',
+					  type: 'noBackground'
+					},
+					{
+						text: '<b>Oui</b>',
+						type: 'button-positive',
+						onTap: function (e) {
+							deleteGroup(groupId);
+						}
+					}
+				]
+			});
 		};
 	});
 	
