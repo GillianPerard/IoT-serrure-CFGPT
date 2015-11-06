3b22148d1539cb65248027b4a90b7d0fa28c9c33
TotoSyfher <ProTechGamer@hotmail.fr>
2015-11-06 14:30:38 +0100

    ConnectedObjectController clean (1/2) and fix

--- CFGPT_API/api/controllers/ConnectedObjectsController.js
+++ CFGPT_API/api/controllers/ConnectedObjectsController.js
@@ -6,14 +6,20 @@
  */
 
 module.exports = {
+<<<<<<< Updated upstream
 
     //Récupération de tous les objets connectés de l'application
     getConnectedObjects : function (req, res) {
+=======
+    
+    getConnectedObjects : function (req, res) { //Récupération de tous les objets connectés de l'application
+>>>>>>> Stashed changes
         ConnectedObjects.find().exec(function (err, connObjects) {
-            if (err) return res.json(400, { err: 'ERROR.' });
-            return res.send(connObjects);
+            if (err) return res.serverError({ "state": "Error when trying access to database" , "error" : err });
+            return res.ok(connObjects);
         });
     },
+<<<<<<< Updated upstream
 
 
     //Récupération d'objet connectés à l'aide de son token
@@ -41,15 +47,39 @@ module.exports = {
         if (!_name || !_tokenObject) return res.json(400, { err: 'PARAMS ERROR.' });
 
         //Création de l'objet
+=======
+    
+    getByToken: function (req, res) {  //Récupération d'objet connectés à l'aide de son token
+        var _tokenObject = req.param('tokenObject');
+        
+        if (!_tokenObject) return res.serverError({ "state" : "Params error" });  //Si il manque le param, on drop.
+        
+        ConnectedObjects.findOneByToken(_tokenObject).exec(function (err, connObject) {
+            if (err) return res.serverError({ "state": "Error when trying acces to database", "error": err });
+            if (!connObject) return res.ok({});     //No result
+            
+            return res.ok(connObject);
+        });
+    },
+    
+    addConnectedObject : function (req, res) {   //Création d'un objet connecté avec les paramètres : name & tokenObject
+        var _name = req.param('name');
+        var _tokenObject = req.param('tokenObject');
+        
+        if (!_name || !_tokenObject) return res.serverError({ "state": "Params error" });         //Si il manque un des params, on drop.
+        
+>>>>>>> Stashed changes
         ConnectedObjects.create({
+            //Création de l'objet
             name: _name,
             token: _tokenObject,
             state: 'Fermé'
         }).exec(function (err, createdConnObj) {
-            if (err) return res.json(400, { err: 'ERROR.' });
-            return res.send(createdConnObj);
+            if (err) return res.serverError({ "State": 'Error when trying add connected object on database', "error": err });
+            return res.ok(createdConnObj);
         });
     },
+<<<<<<< Updated upstream
 
 
     //Suppression d'un objet connecté à l'aide de son token
@@ -73,10 +103,31 @@ module.exports = {
                     if (err) return res.json(400, { err: 'ERROR.' });
 
                     return res.send(connObjToDestroy);
+=======
+    
+    removeByToken: function (req, res) {    //Suppression d'un objet connecté à l'aide de son token
+        var _tokenObject = req.param('tokenObject');
+        
+        if (!_tokenObject) return res.serverError({ "state": 'Params error' });  //Si il manque le param, on drop.
+        
+        ConnectedObjects.findOneByToken(_tokenObject).exec(function (err, connObject) {
+            if (err) return res.serverError({ "state": "Error when trying access to database", "error" : err });
+            if (!connObject) return res.ok({});       //No result
+            
+            ConnectedObjects.update({ token: _tokenObject }, { groups: [], logs: [] }).exec(function (err, connObject) {     //On supprime les liens de l'objet avec les groupes et les logs
+                if (err) return res.serverError({ "state" : "Error when trying update database", "error": err });
+                if (connObject.length == 0) return res.ok({});  //No result
+                
+                ConnectedObjects.destroy({ id: connObject[0]['id'] }).exec(function (err, connObjToDestroy) {   //Enfin, suppression de l'objet concerné
+                    if (err) return res.serverError({ "state": "Error when trying delete on Database", "error": err });
+                    
+                    return res.ok(connObjToDestroy);
+>>>>>>> Stashed changes
                 });
             });
         });
     },
+<<<<<<< Updated upstream
 
     //Récupération des logs d'un objet connecté à l'aide de son token
     logsByToken: function (req, res) {
@@ -100,24 +151,55 @@ module.exports = {
 
 
 
+=======
+    
+    logsByToken: function (req, res) {       //Récupération des logs d'un objet connecté à l'aide de son token
+        var _tokenObject = req.param('tokenObject');
+        
+        if (!_tokenObject) return res.serverError({ "state": 'Param is incorrect' });      //Si il manque le param, on drop.
+        
+        ConnectedObjects.findOneByToken(_tokenObject).exec(function (err, connObject) {
+            if (err) return res.serverError({ "state": "Error when trying access to Database", "error": err });
+            if (!connObject) return res.ok({});     //No result
+            
+            Logs.find({ connectedobject: connObject.id }).populate('user').exec(function (err, logs) {
+                if (err) return res.serverError({ "state": "Error when trying access to database", "error" : err });
+                if (!logs) return res.ok({});      //No result
+                
+                return res.ok(logs);
+            });
+        });
+    },
+    
+>>>>>>> Stashed changes
     reportState: function (req, res) {
         var _state = req.param("state");
         var _tokenObject = req.param("tokenObject");
         if (["Fermé", "Ouvert", "Sonne"].contains(state)) {
             ConnectedObjects.update({ token: _tokenObject }, { state: _state }, function (err, created) {
-                console.log(err + created[0].name);
+                if (err) res.serverError({ "state" : "Error when trying update state on database", "error" : err });
+                res.ok(created);
             });
         }
     },
+<<<<<<< Updated upstream
 
 
+=======
+    
+>>>>>>> Stashed changes
     checkUserValidity: function (req, res) {
         var _token = req.headers.authorization;
         var _tokenObject = req.param("tokenObject");
 
         if (!_token || !_tokenObject) return res.send(400, { "state": "Params missing" });
+<<<<<<< Updated upstream
 
         ConnectedObjects.query('SELECT co.token ' +
+=======
+        
+        ConnectedObjects.query('SELECT co.token ' + 
+>>>>>>> Stashed changes
                                 'FROM connectedobjects co ' +
                                 'LEFT JOIN connectedobjects_groups__groups_connectedobjects cog ON (co.id = cog.connectedobjects_groups) ' +
                                 'LEFT JOIN groups g ON (cog.groups_connectedobjects = g.id) ' +
@@ -125,7 +207,7 @@ module.exports = {
                                 'LEFT JOIN users u ON (gu.`user` = u.id) ' +
                                 'WHERE u.token = "' + _token + '"',
                                 function (err, results) {
-            if (err) return res.send(400,{ "state" : "Erreur when getting information on database", "error" : err });
+            if (err) return res.send(400, { "state" : "Erreur when getting information on database", "error" : err });
             var listToken = [];
             results.forEach(function (factor, index) {
                 listToken.push(factor.token);
