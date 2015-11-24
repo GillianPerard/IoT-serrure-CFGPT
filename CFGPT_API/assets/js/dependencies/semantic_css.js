0d0b735c1c69a5694b63c1f922db0949c282ec1a
Udolwa34 <ludovic.caisso@y-nov.com>
2015-11-24 23:04:38 +0100

    Add js logic for semantic items

--- CFGPT_API/assets/js/dependencies/semantic_css.js
+++ CFGPT_API/assets/js/dependencies/semantic_css.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
   $('.ui.menu a.item').on('click', function() {
       $(this).addClass('active').siblings().removeClass('active');
   });
+  //$('.special.card .image').dimmer({on: 'hover'});
+  //$('.star.rating').rating();
+  $('.card .dimmer').dimmer({on: 'hover'});
 });
